00a37f76225f74d6541814a626a67e3c30bcf67c
Christian Stewart <christian@paral.in>
2022-07-06 20:39:41 -0700

    feat: add typescript bindings (#2)
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ bus/api/controller/controller.pb.ts
@@ -0,0 +1,159 @@
+/* eslint-disable */
+import Long from 'long'
+import { Config as Config1 } from '../api.pb.js'
+import * as _m0 from 'protobufjs/minimal'
+
+export const protobufPackage = 'bus.api.controller'
+
+/** Config configures the API. */
+export interface Config {
+  /** ListenAddr is the address to listen on for connections. */
+  listenAddr: string
+  /** BusApiConfig are options for controller bus api. */
+  busApiConfig: Config1 | undefined
+}
+
+function createBaseConfig(): Config {
+  return { listenAddr: '', busApiConfig: undefined }
+}
+
+export const Config = {
+  encode(
+    message: Config,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
+    if (message.listenAddr !== '') {
+      writer.uint32(10).string(message.listenAddr)
+    }
+    if (message.busApiConfig !== undefined) {
+      Config1.encode(message.busApiConfig, writer.uint32(18).fork()).ldelim()
+    }
+    return writer
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): Config {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input)
+    let end = length === undefined ? reader.len : reader.pos + length
+    const message = createBaseConfig()
+    while (reader.pos < end) {
+      const tag = reader.uint32()
+      switch (tag >>> 3) {
+        case 1:
+          message.listenAddr = reader.string()
+          break
+        case 2:
+          message.busApiConfig = Config1.decode(reader, reader.uint32())
+          break
+        default:
+          reader.skipType(tag & 7)
+          break
+      }
+    }
+    return message
+  },
+
+  // encodeTransform encodes a source of message objects.
+  // Transform<Config, Uint8Array>
+  async *encodeTransform(
+    source: AsyncIterable<Config | Config[]> | Iterable<Config | Config[]>
+  ): AsyncIterable<Uint8Array> {
+    for await (const pkt of source) {
+      if (Array.isArray(pkt)) {
+        for (const p of pkt) {
+          yield* [Config.encode(p).finish()]
+        }
+      } else {
+        yield* [Config.encode(pkt).finish()]
+      }
+    }
+  },
+
+  // decodeTransform decodes a source of encoded messages.
+  // Transform<Uint8Array, Config>
+  async *decodeTransform(
+    source:
+      | AsyncIterable<Uint8Array | Uint8Array[]>
+      | Iterable<Uint8Array | Uint8Array[]>
+  ): AsyncIterable<Config> {
+    for await (const pkt of source) {
+      if (Array.isArray(pkt)) {
+        for (const p of pkt) {
+          yield* [Config.decode(p)]
+        }
+      } else {
+        yield* [Config.decode(pkt)]
+      }
+    }
+  },
+
+  fromJSON(object: any): Config {
+    return {
+      listenAddr: isSet(object.listenAddr) ? String(object.listenAddr) : '',
+      busApiConfig: isSet(object.busApiConfig)
+        ? Config1.fromJSON(object.busApiConfig)
+        : undefined,
+    }
+  },
+
+  toJSON(message: Config): unknown {
+    const obj: any = {}
+    message.listenAddr !== undefined && (obj.listenAddr = message.listenAddr)
+    message.busApiConfig !== undefined &&
+      (obj.busApiConfig = message.busApiConfig
+        ? Config1.toJSON(message.busApiConfig)
+        : undefined)
+    return obj
+  },
+
+  fromPartial<I extends Exact<DeepPartial<Config>, I>>(object: I): Config {
+    const message = createBaseConfig()
+    message.listenAddr = object.listenAddr ?? ''
+    message.busApiConfig =
+      object.busApiConfig !== undefined && object.busApiConfig !== null
+        ? Config1.fromPartial(object.busApiConfig)
+        : undefined
+    return message
+  },
+}
+
+type Builtin =
+  | Date
+  | Function
+  | Uint8Array
+  | string
+  | number
+  | boolean
+  | undefined
+
+export type DeepPartial<T> = T extends Builtin
+  ? T
+  : T extends Long
+  ? string | number | Long
+  : T extends Array<infer U>
+  ? Array<DeepPartial<U>>
+  : T extends ReadonlyArray<infer U>
+  ? ReadonlyArray<DeepPartial<U>>
+  : T extends { $case: string }
+  ? { [K in keyof Omit<T, '$case'>]?: DeepPartial<T[K]> } & {
+      $case: T['$case']
+    }
+  : T extends {}
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>
+
+type KeysOfUnion<T> = T extends T ? keyof T : never
+export type Exact<P, I extends P> = P extends Builtin
+  ? P
+  : P & { [K in keyof P]: Exact<P[K], I[K]> } & Record<
+        Exclude<keyof I, KeysOfUnion<P>>,
+        never
+      >
+
+if (_m0.util.Long !== Long) {
+  _m0.util.Long = Long as any
+  _m0.configure()
+}
+
+function isSet(value: any): boolean {
+  return value !== null && value !== undefined
+}
---
+++ directive/directive.pb.ts
@@ -0,0 +1,380 @@
+/* eslint-disable */
+import Long from 'long'
+import * as _m0 from 'protobufjs/minimal'
+
+export const protobufPackage = 'directive'
+
+/** DirectiveInfo contains directive information in protobuf form. */
+export interface DirectiveInfo {
+  /** Name is the directive name. */
+  name: string
+  /** DebugVals contains the directive debug values. */
+  debugVals: ProtoDebugValue[]
+}
+
+/** DirectiveState contains directive info and state info in protobuf form. */
+export interface DirectiveState {
+  /** Info is the directive info. */
+  info: DirectiveInfo | undefined
+}
+
+/** ProtoDebugValue is a debug value. */
+export interface ProtoDebugValue {
+  /** Key is the debug value key. */
+  key: string
+  /** Values are the debug value values. */
+  values: string[]
+}
+
+function createBaseDirectiveInfo(): DirectiveInfo {
+  return { name: '', debugVals: [] }
+}
+
+export const DirectiveInfo = {
+  encode(
+    message: DirectiveInfo,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
+    if (message.name !== '') {
+      writer.uint32(10).string(message.name)
+    }
+    for (const v of message.debugVals) {
+      ProtoDebugValue.encode(v!, writer.uint32(18).fork()).ldelim()
+    }
+    return writer
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): DirectiveInfo {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input)
+    let end = length === undefined ? reader.len : reader.pos + length
+    const message = createBaseDirectiveInfo()
+    while (reader.pos < end) {
+      const tag = reader.uint32()
+      switch (tag >>> 3) {
+        case 1:
+          message.name = reader.string()
+          break
+        case 2:
+          message.debugVals.push(
+            ProtoDebugValue.decode(reader, reader.uint32())
+          )
+          break
+        default:
+          reader.skipType(tag & 7)
+          break
+      }
+    }
+    return message
+  },
+
+  // encodeTransform encodes a source of message objects.
+  // Transform<DirectiveInfo, Uint8Array>
+  async *encodeTransform(
+    source:
+      | AsyncIterable<DirectiveInfo | DirectiveInfo[]>
+      | Iterable<DirectiveInfo | DirectiveInfo[]>
+  ): AsyncIterable<Uint8Array> {
+    for await (const pkt of source) {
+      if (Array.isArray(pkt)) {
+        for (const p of pkt) {
+          yield* [DirectiveInfo.encode(p).finish()]
+        }
+      } else {
+        yield* [DirectiveInfo.encode(pkt).finish()]
+      }
+    }
+  },
+
+  // decodeTransform decodes a source of encoded messages.
+  // Transform<Uint8Array, DirectiveInfo>
+  async *decodeTransform(
+    source:
+      | AsyncIterable<Uint8Array | Uint8Array[]>
+      | Iterable<Uint8Array | Uint8Array[]>
+  ): AsyncIterable<DirectiveInfo> {
+    for await (const pkt of source) {
+      if (Array.isArray(pkt)) {
+        for (const p of pkt) {
+          yield* [DirectiveInfo.decode(p)]
+        }
+      } else {
+        yield* [DirectiveInfo.decode(pkt)]
+      }
+    }
+  },
+
+  fromJSON(object: any): DirectiveInfo {
+    return {
+      name: isSet(object.name) ? String(object.name) : '',
+      debugVals: Array.isArray(object?.debugVals)
+        ? object.debugVals.map((e: any) => ProtoDebugValue.fromJSON(e))
+        : [],
+    }
+  },
+
+  toJSON(message: DirectiveInfo): unknown {
+    const obj: any = {}
+    message.name !== undefined && (obj.name = message.name)
+    if (message.debugVals) {
+      obj.debugVals = message.debugVals.map((e) =>
+        e ? ProtoDebugValue.toJSON(e) : undefined
+      )
+    } else {
+      obj.debugVals = []
+    }
+    return obj
+  },
+
+  fromPartial<I extends Exact<DeepPartial<DirectiveInfo>, I>>(
+    object: I
+  ): DirectiveInfo {
+    const message = createBaseDirectiveInfo()
+    message.name = object.name ?? ''
+    message.debugVals =
+      object.debugVals?.map((e) => ProtoDebugValue.fromPartial(e)) || []
+    return message
+  },
+}
+
+function createBaseDirectiveState(): DirectiveState {
+  return { info: undefined }
+}
+
+export const DirectiveState = {
+  encode(
+    message: DirectiveState,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
+    if (message.info !== undefined) {
+      DirectiveInfo.encode(message.info, writer.uint32(10).fork()).ldelim()
+    }
+    return writer
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): DirectiveState {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input)
+    let end = length === undefined ? reader.len : reader.pos + length
+    const message = createBaseDirectiveState()
+    while (reader.pos < end) {
+      const tag = reader.uint32()
+      switch (tag >>> 3) {
+        case 1:
+          message.info = DirectiveInfo.decode(reader, reader.uint32())
+          break
+        default:
+          reader.skipType(tag & 7)
+          break
+      }
+    }
+    return message
+  },
+
+  // encodeTransform encodes a source of message objects.
+  // Transform<DirectiveState, Uint8Array>
+  async *encodeTransform(
+    source:
+      | AsyncIterable<DirectiveState | DirectiveState[]>
+      | Iterable<DirectiveState | DirectiveState[]>
+  ): AsyncIterable<Uint8Array> {
+    for await (const pkt of source) {
+      if (Array.isArray(pkt)) {
+        for (const p of pkt) {
+          yield* [DirectiveState.encode(p).finish()]
+        }
+      } else {
+        yield* [DirectiveState.encode(pkt).finish()]
+      }
+    }
+  },
+
+  // decodeTransform decodes a source of encoded messages.
+  // Transform<Uint8Array, DirectiveState>
+  async *decodeTransform(
+    source:
+      | AsyncIterable<Uint8Array | Uint8Array[]>
+      | Iterable<Uint8Array | Uint8Array[]>
+  ): AsyncIterable<DirectiveState> {
+    for await (const pkt of source) {
+      if (Array.isArray(pkt)) {
+        for (const p of pkt) {
+          yield* [DirectiveState.decode(p)]
+        }
+      } else {
+        yield* [DirectiveState.decode(pkt)]
+      }
+    }
+  },
+
+  fromJSON(object: any): DirectiveState {
+    return {
+      info: isSet(object.info)
+        ? DirectiveInfo.fromJSON(object.info)
+        : undefined,
+    }
+  },
+
+  toJSON(message: DirectiveState): unknown {
+    const obj: any = {}
+    message.info !== undefined &&
+      (obj.info = message.info ? DirectiveInfo.toJSON(message.info) : undefined)
+    return obj
+  },
+
+  fromPartial<I extends Exact<DeepPartial<DirectiveState>, I>>(
+    object: I
+  ): DirectiveState {
+    const message = createBaseDirectiveState()
+    message.info =
+      object.info !== undefined && object.info !== null
+        ? DirectiveInfo.fromPartial(object.info)
+        : undefined
+    return message
+  },
+}
+
+function createBaseProtoDebugValue(): ProtoDebugValue {
+  return { key: '', values: [] }
+}
+
+export const ProtoDebugValue = {
+  encode(
+    message: ProtoDebugValue,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
+    if (message.key !== '') {
+      writer.uint32(10).string(message.key)
+    }
+    for (const v of message.values) {
+      writer.uint32(18).string(v!)
+    }
+    return writer
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): ProtoDebugValue {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input)
+    let end = length === undefined ? reader.len : reader.pos + length
+    const message = createBaseProtoDebugValue()
+    while (reader.pos < end) {
+      const tag = reader.uint32()
+      switch (tag >>> 3) {
+        case 1:
+          message.key = reader.string()
+          break
+        case 2:
+          message.values.push(reader.string())
+          break
+        default:
+          reader.skipType(tag & 7)
+          break
+      }
+    }
+    return message
+  },
+
+  // encodeTransform encodes a source of message objects.
+  // Transform<ProtoDebugValue, Uint8Array>
+  async *encodeTransform(
+    source:
+      | AsyncIterable<ProtoDebugValue | ProtoDebugValue[]>
+      | Iterable<ProtoDebugValue | ProtoDebugValue[]>
+  ): AsyncIterable<Uint8Array> {
+    for await (const pkt of source) {
+      if (Array.isArray(pkt)) {
+        for (const p of pkt) {
+          yield* [ProtoDebugValue.encode(p).finish()]
+        }
+      } else {
+        yield* [ProtoDebugValue.encode(pkt).finish()]
+      }
+    }
+  },
+
+  // decodeTransform decodes a source of encoded messages.
+  // Transform<Uint8Array, ProtoDebugValue>
+  async *decodeTransform(
+    source:
+      | AsyncIterable<Uint8Array | Uint8Array[]>
+      | Iterable<Uint8Array | Uint8Array[]>
+  ): AsyncIterable<ProtoDebugValue> {
+    for await (const pkt of source) {
+      if (Array.isArray(pkt)) {
+        for (const p of pkt) {
+          yield* [ProtoDebugValue.decode(p)]
+        }
+      } else {
+        yield* [ProtoDebugValue.decode(pkt)]
+      }
+    }
+  },
+
+  fromJSON(object: any): ProtoDebugValue {
+    return {
+      key: isSet(object.key) ? String(object.key) : '',
+      values: Array.isArray(object?.values)
+        ? object.values.map((e: any) => String(e))
+        : [],
+    }
+  },
+
+  toJSON(message: ProtoDebugValue): unknown {
+    const obj: any = {}
+    message.key !== undefined && (obj.key = message.key)
+    if (message.values) {
+      obj.values = message.values.map((e) => e)
+    } else {
+      obj.values = []
+    }
+    return obj
+  },
+
+  fromPartial<I extends Exact<DeepPartial<ProtoDebugValue>, I>>(
+    object: I
+  ): ProtoDebugValue {
+    const message = createBaseProtoDebugValue()
+    message.key = object.key ?? ''
+    message.values = object.values?.map((e) => e) || []
+    return message
+  },
+}
+
+type Builtin =
+  | Date
+  | Function
+  | Uint8Array
+  | string
+  | number
+  | boolean
+  | undefined
+
+export type DeepPartial<T> = T extends Builtin
+  ? T
+  : T extends Long
+  ? string | number | Long
+  : T extends Array<infer U>
+  ? Array<DeepPartial<U>>
+  : T extends ReadonlyArray<infer U>
+  ? ReadonlyArray<DeepPartial<U>>
+  : T extends { $case: string }
+  ? { [K in keyof Omit<T, '$case'>]?: DeepPartial<T[K]> } & {
+      $case: T['$case']
+    }
+  : T extends {}
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>
+
+type KeysOfUnion<T> = T extends T ? keyof T : never
+export type Exact<P, I extends P> = P extends Builtin
+  ? P
+  : P & { [K in keyof P]: Exact<P[K], I[K]> } & Record<
+        Exclude<keyof I, KeysOfUnion<P>>,
+        never
+      >
+
+if (_m0.util.Long !== Long) {
+  _m0.util.Long = Long as any
+  _m0.configure()
+}
+
+function isSet(value: any): boolean {
+  return value !== null && value !== undefined
+}
