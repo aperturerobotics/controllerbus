3de328645d84248c662ab3fc3da4c186e9bd840b
Christian Stewart <christian@paral.in>
2022-07-30 15:23:15 -0700

    chore: update dependencies
    Signed-off-by: Christian Stewart <christian@paral.in>

--- controller/exec/exec.pb.ts
+++ controller/exec/exec.pb.ts
@@ -2,7 +2,7 @@
 import Long from 'long'
 import { ConfigSet } from '../configset/proto/configset.pb.js'
 import { Info } from '../controller.pb.js'
-import * as _m0 from 'protobufjs/minimal'
+import _m0 from 'protobufjs/minimal.js'
 
 export const protobufPackage = 'controller.exec'
 
