632240038cb70584558f04276a3c475f0d68a917
Christian Stewart <christian@paral.in>
2022-10-10 17:40:44 -0700

    refactor(util): wait: add returnIfIdle parameter
    Signed-off-by: Christian Stewart <christian@paral.in>

--- bus/util_wait.go
+++ bus/util_wait.go
@@ -7,12 +7,14 @@ import (
 )
 
 // ExecWaitValue executes a directive and waits for a value matching the cb.
+// If returnIfIdle is set, returns nil, nil, nil if idle.
 // If cb returns true, nil, returns the value.
 // If checkCb is nil, returns first value.
 func ExecWaitValue[T any](
 	ctx context.Context,
 	b Bus,
 	dir directive.Directive,
+	returnIfIdle bool,
 	checkCb func(val T) (bool, error),
 ) (T, directive.Reference, error) {
 	waitCtx, waitCtxCancel := context.WithCancel(ctx)
@@ -32,7 +34,7 @@ func ExecWaitValue[T any](
 			}
 		}
 	}
-	_, diRef, err := b.AddDirective(
+	di, diRef, err := b.AddDirective(
 		dir,
 		NewCallbackHandler(func(av directive.AttachedValue) {
 			val, valOk := av.GetValue().(T)
@@ -47,12 +49,36 @@ func ExecWaitValue[T any](
 		return empty, nil, err
 	}
 
+	errCh := make(chan error, 1)
+	releaseIdleCallback := di.AddIdleCallback(func(errs []error) {
+		var err error
+		if len(errs) != 0 {
+			for _, rerr := range errs {
+				if rerr != nil {
+					err = rerr
+					break
+				}
+			}
+		}
+		if !returnIfIdle && err == nil {
+			return
+		}
+		select {
+		case errCh <- err:
+		default:
+		}
+	})
+	defer releaseIdleCallback()
+
 	for {
 		select {
 		case <-waitCtx.Done():
 			diRef.Release()
 			var empty T
 			return empty, nil, context.Canceled
+		case err := <-errCh:
+			var empty T
+			return empty, nil, err
 		case val := <-valCh:
 			if checkCb != nil {
 				matched, err := checkCb(val)
