e7346c09592b66f341da34225feec367bc144ac2
Christian Stewart <christian@aperture.us>
2024-02-26 21:33:56 -0800

    feat: add unique list, map xfrm resolver
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- directive/unique-list-resolver.go
+++ directive/unique-list-resolver.go
@@ -58,3 +58,70 @@ func NewUniqueListResolver[K, V comparable](
 		),
 	}
 }
+
+// UniqueListXfrmResolver accepts and de-duplicates a list of keyed objects.
+// The objects are transformed and emitted to the ValueHandler.
+// If an object changes, the old value is removed & replaced.
+//
+// cmp checks if two values are equal.
+// if equal, the old version of the value is used.
+//
+// xfrm transforms the values before emitting them.
+// if xfrm returns false the value is dropped.
+//
+// K is the key type
+// V is the value type
+// O is the type to emit to the resolver
+type UniqueListXfrmResolver[K, V comparable, O any] struct {
+	*unique.KeyedList[K, V]
+}
+
+// NewUniqueListXfrmResolver constructs a new UniqueListXfrmResolver.
+func NewUniqueListXfrmResolver[K, V comparable, O any](
+	getKey func(v V) K,
+	cmp func(k K, a, b V) bool,
+	xfrm func(k K, v V) (O, bool),
+	hnd ValueHandler,
+) *UniqueListXfrmResolver[K, V, O] {
+	valueIds := make(map[K]uint32)
+	return &UniqueListXfrmResolver[K, V, O]{
+		KeyedList: unique.NewKeyedList[K, V](
+			getKey,
+			cmp,
+			func(k K, v V, added bool, removed bool) {
+				if added {
+					xfrm, xfrmOk := xfrm(k, v)
+					if xfrmOk {
+						valID, accepted := hnd.AddValue(xfrm)
+						if accepted {
+							valueIds[k] = valID
+						}
+					}
+					return
+				}
+
+				// removed or changed
+				valID, ok := valueIds[k]
+				if ok {
+					_, _ = hnd.RemoveValue(valID)
+					delete(valueIds, k)
+				}
+				if removed {
+					return
+				}
+
+				// changed
+				xfrm, xfrmOk := xfrm(k, v)
+				if xfrmOk {
+					valID, accepted := hnd.AddValue(xfrm)
+					if accepted {
+						valueIds[k] = valID
+					}
+				}
+				return
+			},
+			// set initial to nil so we generate the AddValue calls.
+			nil,
+		),
+	}
+}
--- directive/unique-map-resolver.go
+++ directive/unique-map-resolver.go
@@ -56,3 +56,68 @@ func NewUniqueMapResolver[K, V comparable](
 		),
 	}
 }
+
+// UniqueMapXfrmResolver accepts and de-duplicates a list of keyed objects.
+// The objects are transformed and emitted to the ValueHandler.
+// If an object changes, the old value is removed & replaced.
+//
+// cmp checks if two values are equal.
+// if equal, the old version of the value is used.
+//
+// xfrm transforms the values before emitting them.
+// if xfrm returns false the value is dropped.
+//
+// K is the key type
+// V is the value type
+// O is the type to emit to the resolver
+type UniqueMapXfrmResolver[K, V comparable, O any] struct {
+	*unique.KeyedMap[K, V]
+}
+
+// NewUniqueMapXfrmResolver constructs a new UniqueMapXfrmResolver.
+func NewUniqueMapXfrmResolver[K, V comparable, O any](
+	cmp func(k K, a V, b V) bool,
+	xfrm func(k K, v V) (O, bool),
+	hnd ValueHandler,
+) *UniqueMapXfrmResolver[K, V, O] {
+	valueIds := make(map[K]uint32)
+	return &UniqueMapXfrmResolver[K, V, O]{
+		KeyedMap: unique.NewKeyedMap[K, V](
+			cmp,
+			func(k K, v V, added bool, removed bool) {
+				if added {
+					xfrm, xfrmOk := xfrm(k, v)
+					if xfrmOk {
+						valID, accepted := hnd.AddValue(xfrm)
+						if accepted {
+							valueIds[k] = valID
+						}
+					}
+					return
+				}
+
+				// removed or changed
+				valID, ok := valueIds[k]
+				if ok {
+					_, _ = hnd.RemoveValue(valID)
+					delete(valueIds, k)
+				}
+				if removed {
+					return
+				}
+
+				// changed
+				xfrm, xfrmOk := xfrm(k, v)
+				if xfrmOk {
+					valID, accepted := hnd.AddValue(xfrm)
+					if accepted {
+						valueIds[k] = valID
+					}
+				}
+				return
+			},
+			// set initial to nil so we generate the AddValue calls.
+			nil,
+		),
+	}
+}
