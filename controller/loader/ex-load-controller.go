00fd08ded3fb6f29a3bfa82564947efa1275e047
Christian Stewart <christian@aperture.us>
2024-06-04 14:36:51 -0700

    feat(loader): add WaitExecControllerRunningTyped function
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- controller/loader/ex-load-controller.go
+++ controller/loader/ex-load-controller.go
@@ -2,6 +2,7 @@ package loader
 
 import (
 	"context"
+	"errors"
 
 	"github.com/aperturerobotics/controllerbus/bus"
 	"github.com/aperturerobotics/controllerbus/controller"
@@ -17,48 +18,58 @@ func WaitExecControllerRunning(
 	dir directive.Directive,
 	disposeCb func(),
 ) (controller.Controller, directive.Instance, directive.Reference, error) {
-	subCtx, subCtxCancel := context.WithCancel(ctx)
-	dispose := func() {
-		subCtxCancel()
-		if disposeCb != nil {
-			disposeCb()
-		}
-	}
-	defer subCtxCancel()
-
-	execValueCh := make(chan ExecControllerValue, 1)
-	di, diRef, err := b.AddDirective(dir, bus.NewCallbackHandler(
-		func(av directive.AttachedValue) {
-			retVal, _ := av.GetValue().(ExecControllerValue)
-			if retVal != nil {
-				select {
-				case <-execValueCh:
-				default:
-				}
-				select {
-				case execValueCh <- retVal:
-				default:
-				}
-			}
-		}, nil, dispose,
-	))
+	execValueCh, di, diRef, err := bus.ExecOneOffWatchCh[ExecControllerValue](b, dir)
 	if err != nil {
 		return nil, nil, nil, err
 	}
 
+	dispose := func() {
+		diRef.Release()
+		if disposeCb != nil {
+			disposeCb()
+		}
+	}
+	defer dispose()
+
 	for {
 		select {
-		case <-subCtx.Done():
-			diRef.Release()
-			return nil, nil, nil, subCtx.Err()
+		case <-ctx.Done():
+			return nil, nil, nil, context.Canceled
 		case val := <-execValueCh:
-			if err := val.GetError(); err != nil {
-				diRef.Release()
+			if val == nil {
+				continue
+			}
+			if err := val.GetValue().GetError(); err != nil {
 				return nil, nil, nil, err
 			}
-			if ctrl := val.GetController(); ctrl != nil {
+			if ctrl := val.GetValue().GetController(); ctrl != nil {
 				return ctrl, di, diRef, nil
 			}
 		}
 	}
+}
+
+// WaitExecControllerRunningTyped executes any directive which yields
+// ExecControllerValue and waits for either a error or success state before
+// returning. Disposed is called if the state leaves RUNNING.
+//
+// If the controller is not of type T an error will be returned and the
+// reference will be released immediately.
+func WaitExecControllerRunningTyped[T controller.Controller](
+	ctx context.Context,
+	b bus.Bus,
+	dir directive.Directive,
+	disposeCb func(),
+) (T, directive.Instance, directive.Reference, error) {
+	var empty T
+	ctrl, di, diRef, err := WaitExecControllerRunning(ctx, b, dir, disposeCb)
+	if err != nil {
+		return empty, di, diRef, err
+	}
+	ctrlt, ok := ctrl.(T)
+	if !ok {
+		diRef.Release()
+		return empty, di, diRef, errors.New("exec controller constructed unexpected controller type")
+	}
+	return ctrlt, di, diRef, nil
 }
