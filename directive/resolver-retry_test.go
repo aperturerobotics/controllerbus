c1afcdc3351cf95cf9e185f264146d9e36acd2e4
Christian Stewart <christian@aperture.us>
2026-09-08 05:28:36 -0700

    test(directive): remove disallowed assertion import
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- directive/resolver-retry_test.go
+++ directive/resolver-retry_test.go
@@ -6,16 +6,25 @@ import (
 	"testing"
 
 	backoff "github.com/aperturerobotics/util/backoff/cbackoff"
-	"github.com/stretchr/testify/require"
 )
 
-type retryTestResolver struct{ err error }
+// retryTestResolver returns a terminal resolver error.
+type retryTestResolver struct {
+	// err is the resolver result.
+	err error
+}
 
-func (r retryTestResolver) Resolve(context.Context, ResolverHandler) error { return r.err }
+// Resolve returns the configured error.
+func (r retryTestResolver) Resolve(context.Context, ResolverHandler) error {
+	return r.err
+}
 
+// TestRetryResolverStopsAtBackoffStop checks terminal backoff propagation.
 func TestRetryResolverStopsAtBackoffStop(t *testing.T) {
 	wantErr := errors.New("resolver failed")
 	resolver := NewRetryResolver(nil, retryTestResolver{err: wantErr}, &backoff.StopBackOff{})
-	err := resolver.Resolve(context.Background(), nil)
-	require.ErrorIs(t, err, wantErr)
+	err := resolver.Resolve(t.Context(), nil)
+	if !errors.Is(err, wantErr) {
+		t.Fatalf("resolver error = %v, want %v", err, wantErr)
+	}
 }
